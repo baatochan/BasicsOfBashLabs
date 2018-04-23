541a562a3e790f1040dc80803ef624aeaabdfd0e
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2018-04-23 09:59:32 +0200

    Modify creating filesscript and add solution to create symlinks

--- lab2/01_create-ln-s.sh
+++ lab2/01_create-ln-s.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-echo 'Creating dir structure'
+echo 'Removing old dir'
 
 rm -rf dir
-mkdir dir
-mkdir dir/a
-mkdir dir/a/b
-mkdir dir/a/b/c
 
-mkdir dir/d
+echo 'Creating dir structure'
 
-mkdir dir/e
+mkdir -p dir/a/b/c
+
+mkdir -p dir/d
+
+mkdir -p dir/e
 
 echo 'Creating files'
 
@@ -19,3 +19,15 @@ echo 'test2' > dir/a/b/c/plik2
 
 echo 'testA' > dir/d/plikA
 echo 'testB' > dir/d/plikB
+
+echo 'Creating symbolic links'
+
+#sciezki wzgledne
+ln -s ../a/b/c/plik1 dir/e/link_plik1
+ln -s ../d/plikA dir/e/link_plikA
+ln -s ../a dir/e/link_do_a
+
+#sciezki bezwzgledne
+ln -s $(pwd)/dir/a/b/c/plik1 dir/e/link_plik1_bez
+ln -s $(pwd)/dir/a/b/c/plik2 dir/e/link_plik2_bez
+ln -s $(pwd)/dir/d/plikB dir/e/link_plikB_bez
